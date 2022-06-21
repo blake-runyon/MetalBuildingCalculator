abb2bb71467905e1603632b97ff2ccb6b5710e59
blake-runyon <johnathan.runyon@snhu.edu>
2022-06-20 22:03:03 -0400

    test route

--- api/index.js
+++ api/index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
 const PORT = 8080
+const conn = require('./connect')
 
-
-app.get('/', (req, res) => {
-    res.send('Working')
+const User = require('./schemas/User')
+app.post('/', async (req, res) => {
+    res.send(await User.create({
+        username: "test", 
+        email: "Bigger Test", 
+        password: "Not Entering a plantext pass lol"
+    }))
+    
 })
 
 app.listen(PORT, () => {
