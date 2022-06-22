90e952f5302b0e21fb6a6867984423050cdbdecb
blake-runyon <johnathan.runyon@snhu.edu>
2022-06-22 16:01:59 -0400

    Edit button for client table

--- ui/src/components/Client/Clients.tsx
+++ ui/src/components/Client/Clients.tsx
@@ -1,21 +1,35 @@
 import AddClient from "./AddClient"
 import { ToggleButton } from "primereact/togglebutton"
 import { useState, useEffect } from 'react'
+import { Button } from 'primereact/button'
 import { ClientService } from "../../services/ClientService";
 import { Column } from "primereact/column";
 import { DataTable } from "primereact/datatable";
+import { Client } from "../../models/Client";
+import { Dialog } from "primereact/dialog";
 
 const Clients = () => {
     const [shown, setShown] = useState(false);
     const [clients, setClients] = useState([])
+    const [editing, setEditing] = useState(false)
+    const [currClient, setCurrClient] = useState<Client>();
     const clientService = new ClientService()
+    const editButtonTemplate = (rowData: Client) => {
+        return <Button label="Edit" onClick={() => { 
+            setCurrClient(rowData)
+            setEditing(!editing)
+         }} />
+    }
 
     useEffect(() => {
         clientService.GetClients().then(data => setClients(data))    
     });
 
     return (
+        
         <>
+        
+        
         <div className={`${shown ? "": "hidden"}`}>
             <AddClient />
         </div>
@@ -29,8 +43,15 @@ const Clients = () => {
             <Column field="address.city" header="City"></Column>
             <Column field="address.state" header="State"></Column>
             <Column field="address.zipcode" header="Zipcode"></Column>
+            <Column header="EDIT" body={editButtonTemplate}></Column>
         </DataTable>
+
+        <Dialog visible={editing} header="Client Details" onHide={() => setEditing(!editing)}>
+            <p>{currClient?.name}</p>
+        </Dialog>  
+        
         </>
+        
     )
 }
 
