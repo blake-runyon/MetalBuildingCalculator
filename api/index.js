ee8f84989d610f4f10da225e147b3bb0614b4ef4
blake-runyon <johnathan.runyon@snhu.edu>
2022-06-21 20:24:08 -0400

    routing for getting all users and adding a user

--- api/index.js
+++ api/index.js
@@ -1,16 +1,26 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 const PORT = 8080
 const conn = require('./connect')
 
+app.use(cors())
+
 const User = require('./schemas/User')
-app.post('/', async (req, res) => {
+
+app.get('/users', async (req, res) => {
+    res.send(await User.find())
+})
+
+app.post('/add-user', async (req, res) => {
     res.send(await User.create({
-        username: "test", 
-        email: "Bigger Test", 
-        password: "Not Entering a plantext pass lol"
+        username: req.body.username, 
+        email: req.body.email, 
+        password: req.body.password
     }))
-    
 })
 
 app.listen(PORT, () => {
