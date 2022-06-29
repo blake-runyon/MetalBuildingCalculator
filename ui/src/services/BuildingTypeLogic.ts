eb4bccd9a93f21585f815bbbc2c145183c2960c0
blake-runyon <johnathan.runyon@snhu.edu>
2022-06-29 17:17:37 -0400

    bug fixes

--- ui/src/services/BuildingTypeLogic.ts
+++ ui/src/services/BuildingTypeLogic.ts
@@ -1,9 +1,16 @@
 export class BuildingTypeLogic {
     GetType(front: boolean, back: boolean, side: boolean) {
-        if((front || back) && !side) {
+
+        
+
+        if(front === false && back === false && side === false) {
+            return "Carport"
+        } else if((front || back) && side === false) {
             return "Barn"
         } else if(side && (front || back)) {
             return "Enclosed Garage"
+        } else {
+            return "Carport"
         }
 
         return "Error: Unknown type"
