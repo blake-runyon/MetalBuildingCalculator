fe798d21ffd6a9904cdd2660507c72804aadc633
blake-runyon <johnathan.runyon@snhu.edu>
2022-07-04 22:10:29 -0400

    Part 1 of login authentication. API side

--- api/index.js
+++ api/index.js
@@ -9,10 +9,20 @@ const conn = require('./connect')
 
 app.use(cors())
 
-app.use('/login', (req, res) => {
-    res.send({
-      token: 'test123'
-    });
+app.use('/login', async (req, res) => {
+    // TODO: Add encryption for password
+
+    // Check if the user exists mongoose method
+    const user = await User.findOne({ username: req.body.username })
+    if(user == null) {
+        res.status(400).send('User does not exist')
+    } else if (user.password !== req.body.password) {
+        res.status(400).send('Password is incorrect')
+    } else {
+        console.log(req.body.username + ' logged in')
+        res.status(200).send({ token: '12345'})
+    }
+
   });
 
 const User = require('./schemas/User')
