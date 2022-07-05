d1f75209cca8f7eb023461f6475370aa3e902b91
blake-runyon <johnathan.runyon@snhu.edu>
2022-07-05 19:12:53 -0400

    Login Form styling

--- ui/src/auth/AuthProvider.tsx
+++ ui/src/auth/AuthProvider.tsx
@@ -12,4 +12,5 @@ export const AuthProvider = (props:{ children: any}) => {
     )
 }
 
+
 export default AuthContext;
--- ui/src/pages/Login.tsx
+++ ui/src/pages/Login.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types'
+import { InputText } from 'primereact/inputtext'
+import { Button } from 'primereact/button'
+import './login.css'
 
 async function loginUser(credentials: any) {
     return fetch('http://localhost:8080/login', {
@@ -26,22 +29,30 @@ export default function Login(props:{ setToken: any }) {
       }
 
   return(
-    <div className="login-wrapper">
-      <h1>Please Log In</h1>
+    <>
+    <div className="bg-grass">
       <form onSubmit={handleSubmit}>
-        <label>
-          <p>Username</p>
-          <input type="text" onChange={e => setUserName(e.target.value)} />
-        </label>
-        <label>
-          <p>Password</p>
-          <input type="password" onChange={e => setPassword(e.target.value)}/>
-        </label>
-        <div>
-          <button type="submit">Submit</button>
+        <div className="flex align-items-center justify-content-center pt-5">
+          <div className="surface-card p-4 shadow-2 border-round w-full lg:w-6">
+            <div className="text-center mb-5">
+                <img src="logo.png" alt="Logo" className='w-full'/>
+                <h2>Please Log In</h2>
+            </div>
+
+            <div className='w-full'>
+                <label htmlFor="username" className="block text-900 font-medium mb-2">Username</label>
+                <InputText id="username" type="text" className="w-full mb-3" onChange={(e) => setUserName(e.target.value)}/>
+
+                <label htmlFor="password" className="w-full block text-900 font-medium mb-2">Password</label>
+                <InputText id="password" type="password" className="w-full mb-3" onChange={(e) => setPassword(e.target.value)} />
+
+                <Button label="Sign In" icon="pi pi-user" className="w-50 text-center" />
+            </div>
+          </div>
         </div>
-      </form>
+      </form>  
     </div>
+    </>
   )
 }
 
